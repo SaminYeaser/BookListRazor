60b4ebf23d79ec9d936c3cd2d2f3ee788967ce9d
Samin <saminyeaser1@gmail.com>
2020-08-25 00:39:30 +0600

    creating constructor

--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,11 @@ namespace BookListRazor.Pages.BookList
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
         }
