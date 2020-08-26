54514f98d35ab5374ce785fc6c50348f56ce9a87
Samin <saminyeaser1@gmail.com>
2020-08-27 02:40:37 +0600

    adding the edit page

--- BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,12 @@ namespace BookListRazor.Pages.BookList
 {
     public class EditModel : PageModel
     {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet(int id)
         {
         }
