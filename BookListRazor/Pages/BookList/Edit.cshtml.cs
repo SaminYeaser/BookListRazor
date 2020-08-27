0325da93e464d6344365d3e035f9d35d2ba98a02
Samin <saminyeaser1@gmail.com>
2020-08-27 16:17:23 +0600

    update is working

--- BookListRazor/Pages/BookList/Edit.cshtml.cs
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace BookListRazor.Pages.BookList
 {
@@ -22,6 +23,24 @@ namespace BookListRazor.Pages.BookList
         public async Task OnGet(int id)
         {
             book = await _db.book.FindAsync(id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDb = await _db.book.FindAsync(book.Id);
+                BookFromDb.Name = book.Name;
+                BookFromDb.AuthorName = book.AuthorName;
+                BookFromDb.ISBN = book.ISBN;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return RedirectToPage();
+            }
         }
     }
 }
